98e8fc68c084e59bc5b44f56fd485faaf3655b36
Rainvar <peterabangan29@gmail.com>
2024-09-04 09:34:38 +0800

    Added second message (Greeting)

--- project.c
+++ project.c
@@ -5,5 +5,7 @@ int main() {
     // printf() displays the string inside quotation
     printf("Hello World Rainvar! Aka Peter Abangan");
 
+    printf("\n Pleased to meeet you!");
+
     return 0;
 }
